825e1c6936d52027eea86d12be9fa843d9f3f653
10-BryanOng <bryanong0102@yahoo.com.sg>
2022-10-06 11:42:52 +0800

    Collision
    Added collision to coins and water

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CoinScript : MonoBehaviour
+{
+    private int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            Score += 10;
+            print("Score");
+        }
+
+        if (other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
